f755a66646f4e1e9779c61b5ec7052f9d46695e0
asantamaria <asantamaira@multiplied.com>
2023-02-22 16:26:47 -0600

    adding the create invoice test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,10 @@
 from test_login import test_login
 from test_logout import test_logout
 from test_invoices import test_invoices
+from test_create_invoices import test_create_invoices
 
 def all_tests(browser):
     test_invoices(browser)
     test_login(browser)
     test_logout(browser)
+    test_create_invoices(browser)
