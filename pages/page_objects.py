abfc73e3eb2ca6ac1ba6f6f16fe140ad554b7394
ASantamaria <asantamaria@multiplied.com>
2023-02-28 19:19:33 -0600

    changing the xpaths to ids

--- pages/page_objects.py
+++ pages/page_objects.py
@@ -7,15 +7,15 @@ url = "http://develop.devcrm.site/"
 title_login = "CecropiaCRM"
 homepage_error = (By.XPATH, '//span[text() = "You must specify a valid username and password."]')
 error_message_login = "You must specify a valid username and password."
-username = (By.XPATH, '//input[@id="user_name"]')
-password = (By.XPATH,'//input[@id="username_password"]')
-login_btn = (By.XPATH, '//*[@id="bigbutton"]')
+username = (By.ID, 'user_name')
+password = (By.ID, 'username_password')
+login_btn = (By.ID, 'bigbutton')
 
 #Homepage
 title_homepage = "Home » CecropiaCRM"
-all_dropdown = (By.XPATH, '//a[@id="grouptab_6"]')
+all_dropdown = (By.ID, 'grouptab_6')
 invoice_option = (By.XPATH, '//*[@id="toolbar"]/ul/li[8]/span[2]/ul/li[31]/a')
-profile_option = (By.XPATH, '//*[@id="with-label"]')
+profile_option = (By.ID, 'with-label')
 logout_option = (By.XPATH, '//div[@class="desktop-bar"]/ul/li/ul/li/a[@id="logout_link"]')
 
 #Invoices
@@ -25,15 +25,15 @@ search_invoice = (By.XPATH, '//*[@id="dataTableInvoicesInformation_filter"]/labe
 
 #Create Invoices
 title_create_invoice = "CREATE » Invoices » CecropiaCRM"
-project_dropdown_create_invoice = (By.XPATH,'//*[@id="select2-project_list-container"]')
+project_dropdown_create_invoice = (By.ID,'select2-project_list-container')
 academy_project_option = (By.XPATH,'//li[text()="Academy Old"]')
-title_field_create_invoice = (By.XPATH,'//*[@id="txt_title"]')
-terms_dropdown_create_invoice = (By.XPATH,'//*[@id="slcterms"]')
+title_field_create_invoice = (By.ID,'txt_title')
+terms_dropdown_create_invoice = (By.ID,'slcterms')
 freeform_radio_create_invoice = (By.XPATH,'//*[@id="pagecontent"]/div/div[2]/div[4]/div/label[1]/input')
 prefilled_with_work_radio_create_invoice = (By.XPATH,'//*[@id="pagecontent"]/div/div[2]/div[4]/div/label[2]/input')
-project_number_create_invoice = (By.XPATH,'//*[@id="txt_project"]')
-status_dropdown_create_invoice = (By.XPATH,'//*[@id="slcstatus"]')
+project_number_create_invoice = (By.ID,'txt_project')
+status_dropdown_create_invoice = (By.ID,'slcstatus')
 status_option_create_invoice = (By.XPATH, '//*[@id="slcstatus"]/option[1]')
-purchase_order_number_create_invoice = (By.XPATH,'//*[@id="txt_purchase_orden"]')
-description_create_invoice = (By.XPATH,'//*[@id="txt_description"]')
-save_btn_create_invoice = (By.XPATH,'//*[@id="btn_create"]')
+purchase_order_number_create_invoice = (By.ID,'txt_purchase_orden')
+description_create_invoice = (By.ID,'txt_description')
+save_btn_create_invoice = (By.ID,'btn_create')
