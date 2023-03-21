74895f3866c341ea8f9a5523bdb496e24deef7fe
ASantamaria <asantamaria@multiplied.com>
2023-03-21 00:12:04 -0600

    adding runner for candidates

---
+++ resources/Run candidates tests.bat
@@ -0,0 +1,3 @@
+cd ..
+pipenv run python -m pytest -s --html=reports/report.html .\tests\runner\test_candidates.py -n 1
+pause
---
+++ tests/runner/test_candidates.py
@@ -0,0 +1,6 @@
+from candidates.test_SCRM_26_candidate import test_SCRM_26_candidate
+# from test_create_invoices import test_create_invoices
+# from test_invoices_search import test_invoices_search
+
+def all_tests(browser):
+    test_SCRM_26_candidate(browser)
