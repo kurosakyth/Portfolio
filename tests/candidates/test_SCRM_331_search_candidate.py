343480a8bdd922479f9b24984ffd5651c526a3a0
ASantamaria <asantamaria@multiplied.com>
2023-03-22 00:30:58 -0600

    adding common code

--- tests/candidates/test_SCRM_331_search_candidate.py
+++ tests/candidates/test_SCRM_331_search_candidate.py
@@ -1,6 +1,6 @@
 import pages.locators as page_object
 from pages.functions import actions
-from pages.common import login
+import pages.common as common
 import credentials.credentials as account
 import time
 
@@ -12,18 +12,12 @@ def test_SCRM_331_search_candidate(browser):
 #LOGIN & HOMEPAGE
 
     # Log in to the page with valid credentials.
-    login(webdriver, account.username_valid, account.password_valid, page_object.title_homepage)
+    common.login(webdriver, account.username_valid, account.password_valid, page_object.title_homepage)
 
-    # Mouse hover and click all.
-    webdriver.hover_and_click_element(page_object.all_dropdown)
+    #CANDIDATE
 
-    # Click the candidates option on All.
-    webdriver.click_button(page_object.candidate_option)
-
-#CANDIDATES
-
-    # Validate the Candidates title page.
-    webdriver.compare_title(page_object.title_candidates)
+    # Go to the Candidates page.
+    common.all_option_selector(webdriver)
 
     # Search a specific word on the search input.
     webdriver.send_keys_to_element(page_object.search_candidate, "Eddy, Cortez")
