e80c997742f908b08909744bb9f31d7389384e38
ASantamaria <asantamaria@multiplied.com>
2023-04-06 17:15:09 -0600

    adding select candidate SCRM 120

---
+++ tests/candidates/test_SCRM_120_select_candidate.py
@@ -0,0 +1,59 @@
+import pages.locators as page_object
+from pages.functions import actions
+import pages.common as common
+import credentials.credentials as account
+import time
+
+def test_SCRM_120_select_candidate(browser):
+    
+    # Using the fixture configuration run the browser.
+    webdriver = actions(browser)
+
+#LOGIN & HOMEPAGE
+
+    # Log in to the page with valid credentials.
+    common.login(webdriver, account.username_valid, account.password_valid, page_object.title_homepage)
+
+#CANDIDATE
+
+    # Go to the Candidates page.
+    common.all_option_selector(webdriver)
+
+    # Search for a Candidate.
+    webdriver.send_keys_to_element(page_object.search_candidate, 'Walden')
+
+    # Click on the candidate searched.
+    webdriver.click_button(page_object.result_searched_test_candidate)
+
+    # Verify the candidate title of the page.
+    webdriver.compare_title(page_object.title_walden_candidate_view)
+
+    # Display the accounts subpanel.
+    webdriver.click_button(page_object.candidate_accounts_view_candidate)
+
+#SELECT A CANDIDATE
+
+    # Click on the Select accounts to open the new window.
+    webdriver.click_button(page_object.select_accounts_view_candidate)
+
+    # Get a list of all the open windows
+    window_handles = browser.window_handles
+
+    # Switch to the latest window (the new window)
+    latest_window = window_handles[-1]
+    browser.switch_to.window(latest_window)
+
+    # On the new window search for "qa".
+    webdriver.send_keys_to_element(page_object.search_input_select_accounts_view_candidate, 'qa')
+    
+    # Click on the select button.
+    webdriver.click_button(page_object.search_button_select_accounts_view_candidate)
+
+    # Select the account to link on.
+    webdriver.click_button(page_object.qa_option_table_select_accounts_view_candidate)
+
+#CREATE A CANDIDATE
+
+    
+
+    time.sleep(10)
